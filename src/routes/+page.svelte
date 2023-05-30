3434e301dd21e362adb0735a0ca00cadd03a3ac8
bheemesh1706 <bheemeshbala.71@gmail.com>
2023-05-30 14:09:45 +0530

    Addes type for the script tag

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,66 +1,50 @@
-<script>
+<script lang="ts">
 
+	// Capture screenshot of a specific area
+	function captureScreenshot(x:number, y:number, width:number, height:number) {
+		navigator.mediaDevices
+			.getDisplayMedia({ video: true })
+			.then((stream) => {
+				const videoTrack = stream.getVideoTracks()[0];
+				const videoElement = document.createElement('video');
+				videoElement.srcObject = new MediaStream([videoTrack]);
 
+				videoElement.onloadedmetadata = () => {
+					const videoWidth = videoElement.videoWidth;
+					const videoHeight = videoElement.videoHeight;
 
+					const canvas = document.createElement('canvas');
+					canvas.width = width;
+					canvas.height = height;
 
+					const context = canvas.getContext('2d');
 
+					context?.drawImage(videoElement, x, y, width, height, 0, 0, width, height);
 
-// Capture screenshot of a specific area
-/**
-	 * @param {number} x
-	 * @param {number} y
-	 * @param {number} width
-	 * @param {number} height
-	 */
-// Capture screenshot of a specific area
-function captureScreenshot(x, y, width, height) {
-  navigator.mediaDevices.getDisplayMedia({ video: true })
-    .then((stream) => {
-      const videoTrack = stream.getVideoTracks()[0];
-      const videoElement = document.createElement('video');
-      videoElement.srcObject = new MediaStream([videoTrack]);
+					canvas.toBlob((blob) => {
+						const screenshotUrl = URL.createObjectURL(blob as Blob);
 
-      videoElement.onloadedmetadata = () => {
-        const videoWidth = videoElement.videoWidth;
-        const videoHeight = videoElement.videoHeight;
+						// Open the screenshot in a new window/tab
+						window.open(screenshotUrl);
 
-        const canvas = document.createElement('canvas');
-        canvas.width = width;
-        canvas.height = height;
+						// Clean up the resources
+						URL.revokeObjectURL(screenshotUrl);
+					}, 'image/png');
 
-        const context = canvas.getContext('2d');
+					// Stop the video stream and clean up resources
+					stream.getTracks().forEach((track) => track.stop());
+				};
 
-        context?.drawImage(
-          videoElement,
-          x, y, width, height,
-          0, 0, width, height
-        );
+				videoElement.play();
+			})
+			.catch((error) => {
+				console.error('An error occurred while accessing the screen:', error);
+			});
+	}
 
-        canvas.toBlob((blob) => {
-
-          const screenshotUrl = URL.createObjectURL(blob);
-
-          // Open the screenshot in a new window/tab
-          window.open(screenshotUrl);
-
-          // Clean up the resources
-          URL.revokeObjectURL(screenshotUrl);
-        }, 'image/png');
-
-        // Stop the video stream and clean up resources
-        stream.getTracks().forEach((track) => track.stop());
-      };
-
-      videoElement.play();
-    })
-    .catch((error) => {
-      console.error('An error occurred while accessing the screen:', error);
-    });
-}
-	
 	function handleClick() {
 		// capture()
-    captureScreenshot(0, 0, 500, 500);
+		captureScreenshot(0, 0, 500, 500);
 	}
 </script>
 
