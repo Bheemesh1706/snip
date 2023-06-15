9a00e3926dadb9571e7f25e3ab1e1d4db44d1554
bheemesh1706 <bheemeshbala.71@gmail.com>
2023-06-15 15:36:21 +0530

    Removed the duplicate rectangles

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,19 +1,17 @@
 <script lang="ts">
-	let mouse = {};
-	var canvasOffset = 0;
-	var canvasx = 0;
-	var canvasy = 0;
-	var last_mousex = 0;
-	var last_mousey = 0;
+	var startX=0;
+	var startY=0;
 	var mousex = 0;
 	var mousey = 0;
 	var mousedown = false;
-	var shapes: any[] = [];
 	var width: number;
 	var height: number;
-	// make var col a global variable
-	var col = 'black';
-	var ad = '';
+	var offsetX=0;
+	var offsetY=0;
+	var scrollX=0;
+	var scrollY=0;
+	// make var color a global variable
+	var color = 'black';
 
 	// Capture screenshot of a specific area
 	function captureScreenshot(x: number, y: number, width: number, height: number) {
@@ -59,63 +57,44 @@
 
 	//Handle mouse event
 	const handleMouseDown = (event: MouseEvent) => {
-		last_mousex = event.clientX - canvasx;
-		last_mousey = event.clientY - canvasy;
+		event.preventDefault();
+    	event.stopPropagation();
+		const canvas = document.getElementById('snip') as HTMLCanvasElement;
+		offsetX = canvas?.offsetLeft as number;
+		offsetY= canvas?.offsetTop as number;
+		startX =  event.clientX - offsetX;
+		startY = event.clientY - offsetY;
+		scrollX= canvas?.scrollLeft as number;
+		scrollY= canvas?.scrollTop as number;
+		console.log(offsetX,offsetY,scrollX,scrollY,startX,startY);
 		mousedown = true;
 	};
 
 	const handleMouseUp = (event: MouseEvent) => {
-		const lastPos = {
-			lastMouseX: last_mousex,
-			lastMouseY: last_mousey,
-			rectWidth: width,
-			rectHeight: height,
-			color: col,
-			name: ad
-		};
-		shapes.push(lastPos);
+		event.preventDefault();
+    	event.stopPropagation();
 		mousedown = false;
 	};
 
 	const handleMouse = (event: MouseEvent) => {
-		mousex = event.clientX - canvasx;
-		mousey = event.clientY - canvasy;
-
+		event.preventDefault();
+    	event.stopPropagation();
+		mousex = event.clientX - offsetX;
+		mousey = event.clientY - offsetY;
 		if (mousedown) {
 			var canvas = document.getElementById('snip') as HTMLCanvasElement;
 			var ctx = canvas?.getContext('2d');
-			ctx?.clearRect(0, 0, canvas.width, canvas.height);
-			width = mousex - last_mousex;
-			height = mousey - last_mousey;
-			col = 'black';
-			if (shapes.length > 0 && ctx) {
-				for (var i in shapes) {
-					// for every shape in the shapes array beginPath
-					ctx.beginPath();
-					//set the color of the stroke
-					ctx.strokeStyle = shapes[i].color;
-					//draw the rect
-					ctx.rect(
-						shapes[i].lastMouseX,
-						shapes[i].lastMouseY,
-						shapes[i].rectWidth,
-						shapes[i].rectHeight
-					);
-					ctx.fillText(
-						shapes[i].name,
-						shapes[i].rectWidth - shapes[i].lastMouseX,
-						shapes[i].rectHeight - shapes[i].lastMouseY
-					);
-					ctx.stroke();
-				}
-			}
+			ctx?.clearRect(0,0, canvas.width, canvas.height);
+			color = 'black';
+			width= mousex - startX;
+			height= mousey - startY;
+
 			//for the new rect beginPath
 			if (ctx) {
 				ctx.beginPath();
-				ctx.rect(last_mousex, last_mousey, width, height);
-				ctx.strokeStyle = col;
-				ctx.lineWidth = 3;
-				ctx.fillText(ad, 100, 100);
+				ctx.strokeRect(startX,startY, width , height );;
+				ctx.strokeStyle = color;
+				ctx.lineWidth = .1;
 				ctx.stroke();
 			}
 		}
@@ -128,8 +107,8 @@
 		element.style.position = 'absolute';
 		element.style.top = '0';
 		element.style.left = '0';
-		element.style.height = '100vh';
-		element.style.width = '100vw';
+		element.style.height = '100%';
+		element.style.width = '100%';
 		element.style.zIndex = '10';
 		element.id = 'snip';
 		element.addEventListener('mousedown', handleMouseDown);
@@ -151,14 +130,4 @@
 	</button>
 </div>
 
-<style>
-	.snip {
-		position: absolute;
-		top: 0;
-		left: 0;
-		z-index: 10;
-		background-color: transparent;
-		width: 100vw;
-		height: 100vh;
-	}
-</style>
+
